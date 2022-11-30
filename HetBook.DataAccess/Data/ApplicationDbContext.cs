368e06669f8d9c39dc941225144f0de26c840bb9
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 18:54:21 -0500

    Added migration AddCategoryToDb in DataAccess

--- HetBook.DataAccess/Data/ApplicationDbContext.cs
+++ HetBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HetBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace HetBookStore.DataAccess.Data
             : base(options)
         {
         }
+            public DbSet<Category> Categories { get; set; }
     }
 }
