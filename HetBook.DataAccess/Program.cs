fc77b02e98e61723920bb93b8beb495beaadc9d8
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 15:37:21 -0500

    All 3 project added

---
+++ HetBook.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HetBook.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
