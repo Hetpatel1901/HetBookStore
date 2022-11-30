e185bb5b4e85f1ddf89f5cf6b82e652f577fca85
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 16:48:26 -0500

    errors

---
+++ HetBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HetBookStore.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
