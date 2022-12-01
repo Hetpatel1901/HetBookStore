1d3ecd4ef0060165f14988f68f6fa76127e3f6f5
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 22:34:03 -0500

    Added Product.cs in models

--- HetBook.DataAccess/Data/ApplicationDbContext.cs
+++ HetBook.DataAccess/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace HetBookStore.DataAccess.Data
         }
             public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
