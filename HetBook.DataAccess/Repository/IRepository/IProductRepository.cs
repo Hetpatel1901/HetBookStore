a8d4ca05d8a99b19d2f74bad1694082291ade777
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 22:42:05 -0500

    Created product and iproduct repository

---
+++ HetBook.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HetBook.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetBook.DataAccess.Repository.IRepository
+{
+
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
+
