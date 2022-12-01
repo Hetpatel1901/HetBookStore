15dd352ae68c400d4d4a90560bb0329207865c1f
Hetpatel1901 <W0805024@myscc.ca>
2022-11-30 21:40:22 -0500

    Created CoverType

---
+++ HetBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,37 @@
+﻿using HetBook.DataAccess.Repository.IRepository;
+using HetBook.Models;
+using HetBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetBook.DataAccess.Repository
+{
+
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            // use .NET LINQ to retrieve the first or default category object
+            // then pass the id as a generic entity which matches the Category ID
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+
+            }
+
+        }
+    }
+
+
+}
---
+++ HetBook.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HetBook.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetBook.DataAccess.Repository.IRepository
+{
+
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType covertype);
+    }
+
+}
